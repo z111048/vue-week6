917c804707ca0c2c70c4c7a6a760ce752478cab6
Yu-Chuan, Ciou <z111048@gmail.com>
2024-03-25 01:03:51 +0800

    Updates

---
+++ assets/AboutView-4mwkRbp5.js
@@ -0,0 +1,1 @@
+import{_ as e,c as o,o as t}from"./index-rPn9ARY-.js";const s={};function c(n,r,a,p,_,i){return t(),o("h1",null,"This is About page.")}const f=e(s,[["render",c]]);export{f as default};
---
+++ assets/OrderView-gAPDNEKw.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as r}from"./index-rPn9ARY-.js";const n={};function o(t,s){return r(),c("h1",null,"訂單列表")}const a=e(n,[["render",o]]);export{a as default};
